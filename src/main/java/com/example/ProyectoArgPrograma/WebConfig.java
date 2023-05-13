de48c3fbcfffd427d1f9b1ead5e628895808a966
FlorFuentes <flopy_200412@hotmail.com>
2023-05-13 00:46:00 -0300

    agregue url en la configuracion web

--- src/main/java/com/example/ProyectoArgPrograma/WebConfig.java
+++ src/main/java/com/example/ProyectoArgPrograma/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("https://portfolio-argprograma-6ed7c.web.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true)
